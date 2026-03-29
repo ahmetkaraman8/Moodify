7d39f89a58e01b718dd21aa1255c733eee2c91bb
ahmetkaraman8 <ahmetkaraman1015@gmail.com>
2026-03-29 18:56:52 +0300

    fonts, typography added and theme moved in src

---
+++ src/theme/typography.ts
@@ -0,0 +1,20 @@
+export const typography = {
+  fontSize: {
+    xxs: 8,
+    xs: 10,
+    sm: 12,
+    xmd: 14,
+    md: 16,
+    lg: 20,
+    xl: 22,
+    xxl: 24,
+    xxxl: 32,
+  },
+
+  fontFamily: {
+    regular: 'Poppins-Regular',
+    medium: 'Poppins-Medium',
+    semiBold: 'Poppins-SemiBold',
+    bold: 'Poppins-Bold',
+  },
+};
